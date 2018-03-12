5bff3d9e289a49ebdac389e800668a53454a4fde
Juancarlos <juan.granda@tecsup.edu.pe>
2018-03-12 18:04:55 -0500

    hola

---
+++ formulario.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<title>Formulario PHP</title>
+</head>
+<body>
+
+    <form method="POST" action="sumar.php">
+        <p>Valor 1:<input type="text" name="T1" size="20"> </p>
+        <p>Valor 2:<input type="text" name="T2" size="20"> </p>
+        <p><input type="submit" name="B1" value="Sumar" size="20"> </p>
+    </form>
+
+</body>
+</html>
