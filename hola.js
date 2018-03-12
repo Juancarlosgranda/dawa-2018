da800029c8ed146694139261ed707a52e5fecc56
Juancarlos <juan.granda@tecsup.edu.pe>
2018-03-12 18:48:29 -0500

    cambios

---
+++ hola.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var manejador = function (solicitud, respuesta){
+    console.log('Conexion entrante');
+    respuesta.end('hola mundo!');
+}
+var servidor = http.createServer(manejador);
+servidor.listen(8080);
