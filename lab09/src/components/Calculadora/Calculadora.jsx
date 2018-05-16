70f61f8dc6caa4e4925d4ac404b5602d5e30af45
Juancarlos <juan.granda@tecsup.edu.pe>
2018-05-16 16:10:18 -0500

    Lab09 terminado, con tarea y conclusiones

--- lab09/src/components/Calculadora/Calculadora.jsx
+++ lab09/src/components/Calculadora/Calculadora.jsx
@@ -2,58 +2,75 @@ import React, { Component } from 'react';
 import './Calculadora.css'
 export default class Contador extends Component {
     state = {
-        cont: this.props.valor,
-        signo:""
-    }
-    disminuirHandler = () => {
+        pass:0,
+        cont: "",
+        signo:"",
+        }
+   
+    aumentarHandler = (e) => {     
+        var input = e.target.value
         this.setState({
-            cont: this.state.cont - 1
-        })
-    }
-    aumentarHandler = () => {
-        alert('this is:', this);
-        // var input = this.refs.myInput;
-        // var inputValue = input.value;
-        // this.setState({
-        //     cont: this.state.cont + inputValue
+             cont: this.state.cont + input
             
-        // })
+         })
     }
-    resetHandler = () => {
+    signoHandler = (e) => {
+        var input = e.target.value
         this.setState({
-            cont: this.props.valor
+            pass: parseInt(this.state.cont),
+            cont: "",
+            signo: input
+
         })
+    }
+    operarHandler = () => {
+        var resultado=0;
+        var operando2 = parseInt(this.state.cont)
+        if (this.state.signo === "+") {
+            resultado = this.state.pass + operando2
+        }
+        else if (this.state.signo === "-") {
+            resultado = this.state.pass - operando2
+        } else if (this.state.signo === "*") {
+            resultado = this.state.pass * operando2
+        } else if (this.state.signo === "/") {
+            resultado = this.state.pass / operando2
+        }else{
+            resultado = this.state.pass * ((operando2) / 100)
+        }
+        this.setState({
+            pass: "",
+            cont: resultado,
+            signo: ""
+
+        }) 
     }
     render() {
-        let advertencia = (<p>Iniciamos con un número mayor a 5!</p>)
-        if (this.props.valor <= 5) {
-            advertencia = null
-        }
         return (<div>
             <form name="calculator">
                 <h3>Calculadora </h3>
                 <p>Haz algun calculo para ver el resultado.</p>
                 <input type="textfield" name="ans" value={this.state.cont}/>
                 <br/>
-                <input type="button" value="1" ref="myInput" onClick={(e) =>this.aumentarHandler("1",e)}/>
-                <input type="button" value="2" ref="myInput" onClick={this.aumentarHandler}/>
-                <input type="button" value="3" ref="myInput" onClick={this.aumentarHandler}/>
-                <input type="button" value="+" onClick="document.calculator.ans.value+='+'"/>
+                <input type="button" value="1" onClick={this.aumentarHandler}/>
+                <input type="button" value="2"  onClick={this.aumentarHandler}/>
+                <input type="button" value="3"  onClick={this.aumentarHandler}/>
+                <input type="button" value="+" onClick={this.signoHandler}/>
                 <br/>
-                <input type="button" value="4" ref="myInput" onClick={this.aumentarHandler}/>
-                <input type="button" value="5" onClick="document.calculator.ans.value+='5'"/>
-                <input type="button" value="6" onClick="document.calculator.ans.value+='6'"/>
-                <input type="button" value="-" onClick="document.calculator.ans.value+='-'"/>
+                <input type="button" value="4" onClick={this.aumentarHandler}/>
+                <input type="button" value="5" onClick={this.aumentarHandler}/>
+                <input type="button" value="6" onClick={this.aumentarHandler}/>
+                <input type="button" value="-" onClick={this.signoHandler}/>
                 <br/>
-                <input type="button" value="7" onClick="document.calculator.ans.value+='7'"/>
-                <input type="button" value="8" onClick="document.calculator.ans.value+='8'"/>
-                <input type="button" value="9" onClick="document.calculator.ans.value+='9'"/>
-                <input type="button" value="*" onClick="document.calculator.ans.value+='*'"/>
+                <input type="button" value="7" onClick={this.aumentarHandler}/>
+                <input type="button" value="8" onClick={this.aumentarHandler}/>
+                <input type="button" value="9" onClick={this.aumentarHandler}/>
+                <input type="button" value="*" onClick={this.signoHandler}/>
                 <br/>
-                <input type="button" value="0" onClick="document.calculator.ans.value+='0'"/>
-                <input type="reset" value="c" onClick={this.resetHandler}/>
-                <input type="button" value="/" onClick="document.calculator.ans.value+='/'"/>
-                <input type="button" value="=" onClick="document.calculator.ans.value=eval(document.calculator.ans.value)"/>
+                <input type="button" value="0" onClick={this.aumentarHandler}/>
+                <input type="reset" value="%" onClick={this.signoHandler}/>
+                <input type="button" value="/" onClick={this.signoHandler}/>
+                <input type="button" value="=" onClick={this.operarHandler}/>
             </form>
         </div>
         )
